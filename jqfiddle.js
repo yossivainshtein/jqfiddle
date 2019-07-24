d458c0d1b26b6a6c2819ed5877e0fbde17b941ad
Yossi <yossiv@yahoo-inc.com>
2019-07-24 21:55:13 +0300

    Removed old code + fixed special characters cases

--- jqfiddle.js
+++ jqfiddle.js
@@ -42,8 +42,6 @@ function createCollapseBox(child_id) {
     return svg;
 }
 
-
-
 class TreeNode {
     constructor() {
         this.children = {};
@@ -113,8 +111,8 @@ class TreeNode {
 function escapeValueForJq(value, isArray) {
     if (isArray) {
         return `[${value}]`;
-    } else if (value.match(/-/g)) {
-        return `.["${value}"]`;
+    } else if (value.match(/.*[^a-zA-Z0-9].*/g)) {
+        return `."${value}"`;
     } else {
         return `.${value}`;
     }
@@ -147,84 +145,10 @@ let selectedPaths_old = new Set();
 toggleHeirarchy = function(heirarchy) {
     return function() {
         selectedPaths.toggle(heirarchy);
-        // if (selectedPaths_old.has(heirarchy)) {
-        //     selectedPaths_old.delete(heirarchy);
-        // } else {
-        //     selectedPaths_old.add(heirarchy);
-        // }
 
         let str = selectedPaths.getJqLine();
         document.getElementById("result-line").innerText = str;
     }
-}
-
-function calculateJQLine(paths) {
-    let flat =  calcFlatJQLine(paths);
-    let obj = calculateObjectJQLine(paths);
-    return obj;
-}
-
-function calcFlatJQLine(paths, addNames) {    
-    let pathLines = new Set();
-
-    paths.forEach(path => pathLines.add(calcHeirarchyLine(path, addNames)));
-    let line = Array.from(pathLines).join(", ");
-    return line;
-}
-
-function calculateObjectJQLine(paths) {
-    let line = '.';
-    if (paths.size == 1) {
-        return calcFlatJQLine(paths);
-    }
-
-    var processedPaths = [];
-
-
-    for (var path of paths) {
-        if (processedPaths.indexOf(path) >= 0) {
-            continue;
-        }
-        var currentElementPaths = [];
-
-        var firstElementPart = calcHeirarchyLine([path[0]]);
-        for (var path2 of paths) {
-            if (calcHeirarchyLine([path2[0]]) == firstElementPart) {
-                currentElementPaths.push(path2.slice(1));
-                processedPaths.push(path2);
-            }
-        } 
-        if (currentElementPaths.length > 1) {
-            var rest = calculateObjectJQLine(currentElementPaths);
-            if (rest.startsWith('{')) {
-                line = `${firstElementPart} | ${calculateObjectJQLine(currentElementPaths)}`
-            } else {
-                line = `${firstElementPart}${calculateObjectJQLine(currentElementPaths)}`
-            }
-        } else {
-            line = `{ ${calcFlatJQLine(paths, true)} }`
-            break;
-        }
-    }
-
-    return line;
-}
-
-function calcHeirarchyLine(path, addNames) {
-    var line = '';
-    for (var element of path) {
-        if (typeof(element) == 'number') {
-            line = `${line}[]`;
-        } else if (element.match(/-/g)) {
-            line = `${line}["${element}"]`;
-        } else {
-            line = `${line}.${element}`;
-        }
-    }
-    if (addNames) {
-        line = `${path[path.length-1]}: ${line}`
-    }
-    return line;
 }
 
 function createJsonTree(obj, current_heirarchy) {
